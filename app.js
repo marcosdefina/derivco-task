34c961f8251fc1f8439ed8db06de617445197e84
Marcos Defina <marcosdefina@gmail.com>
2019-06-19 11:50:15 +0300

    set no debug to show off progress

--- app.js
+++ app.js
@@ -159,7 +159,7 @@ function onAssetsLoaded() {
         playerBalance -= 1;
         updateBalance(playerBalance);
 
-        debugMode = true;
+        debugMode = false;
         var firstReelMoves = 1;
         var secondReelMoves = 3; // n + firstReelMoves
         var thirdReelMoves = 1; // n times secondReelMoves
@@ -175,8 +175,8 @@ function onAssetsLoaded() {
         } else {
             for (let i = 0; i < reels.length; i++) {
                 const r = reels[i];
-                const extra = debugMode ? Math.floor(Math.random() * firstReelMoves) : firstReelMoves;
-                const target = r.position + (1 + extra) + i * secondReelMoves;
+                const extra = Math.floor(Math.random() * 3);
+                const target = r.position + (6 + extra) + i * 15;
                 const time = 2500 + i * 600 + extra * 600;
                 tweenTo(r, 'position', target, time, backout(0.5),
                     null, i === reels.length - 1 ? reelsComplete : null);
